1ebd255130e44dd5d41db2592a09cb9e553125d7
marissa-kate <47466425+marissa-kate@users.noreply.github.com>
2026-09-13 11:54:35 -0400

    Remove comment from test case for clarity

--- tests/basic-parser.test.ts
+++ tests/basic-parser.test.ts
@@ -9,7 +9,7 @@ test("parseCSV yields arrays", async () => {
   expect(results).toHaveLength(5);
   expect(results[0]).toEqual(["name", "age"]);
   expect(results[1]).toEqual(["Alice", "23"]);
-  expect(results[2]).toEqual(["Bob", "thirty"]); // why does this work? :(
+  expect(results[2]).toEqual(["Bob", "thirty"]); 
   expect(results[3]).toEqual(["Charlie", "25"]);
   expect(results[4]).toEqual(["Nim", "22"]);
 });
